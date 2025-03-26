606d2aa8d626879693d27834e174646f11bbaca7
salozubz <gibzaccess487@gmail.com>
2025-03-26 21:41:18 +0300

    Update whats_db_merge.sh
    Minor fixes

--- whats_db_merge.sh
+++ whats_db_merge.sh
@@ -1,6 +1,9 @@
+#/!bin/bash
+
 ##here put the full paths to the databases in quotes. Multiple databases can be merged. As long as they have a recent schema. From 2022 or later
 
-dbs=("path_to_db_1" "path_to_db_2")
+dbs=("path_to_db1" "path_to_db2")
+
 
 #we need to use the latest database as the schema for the merged database. To get the latest database we have to find the one with the newest timestamp. This method can be overridden by directly assigning the database you desire as the base_db variable
 
@@ -52,7 +55,7 @@ output="${outputdir}/msgstore.db"
 
 #remove any preexisting wal and shm files before beginning which might corrupt the process 
 
-find "${outputdir}" -maxdepth 1 -type f \( -name "msgstore.db-wal" -o -name "msgstore.db-shm" -o -name "msgstore_copy.db-wal" -o -name "msgstore_copy.db-shm" \) -exec rm {} +
+find "${outputdir}" -maxdepth 1 -type f \( -name "msgstore.db-wal" -o -name "msgstore.db-shm" -o -name "msgstore_copy.db-wal" -o -name "msgstore_copy.db-shm" \) -delete
 
 
 #Copy of the base database which will be used in the final loop for sorting messages according to timestamp But first we remove triggers after backing them up then empty the table.
@@ -65,17 +68,18 @@ sqlite3 "$output" "vacuum;"
 
 echo "clearing triggers"
 
-#backup
-triggers=$(sqlite3 "$output" "select sql from sqlite_master where type='trigger';")
+#backup triggers
+triggers=$(sqlite3 "$output" "select name, sql from sqlite_master where type='trigger';")
 
-#delete
-sqlite3 "$output" "select name from sqlite_master where type='trigger';" | while IFS= read -r trigger; do
- sqlite3 "$output" "drop trigger if exists ${trigger};"
+
+#delete triggers
+printf "%s\n" "$triggers" | while read -r line; do
+  sqlite3 "$output" "drop trigger if exists ${line%|*};" 2>/dev/null
 done
 
 echo "clearing tables"
-IFS=$'\n' read -rd '' -a base_tables <<< $(sqlite3 "$output" "select name from sqlite_master where type='table' and name not like '%fts%' and name not like '%view%';")
-base_tables_str=$(sqlite3 "$output" "select group_concat('''' || name || '''') from sqlite_master where type='table' and name not like '%fts%' and name not like '%view%' and name not in ('sqlite_sequence','android_metadata');")
+IFS=$'\n' read -rd '' -a base_tables <<< $(sqlite3 "$output" "select name from sqlite_master where type='table' and name not like '%fts%' and name not like '%view%' and name != 'props';")
+base_tables_str=$(sqlite3 "$output" "select group_concat('''' || name || '''') from sqlite_master where type='table' and name not like '%fts%' and name not like '%view%' and name not in ('sqlite_sequence','android_metadata','props');")
 
 
 #this script classifies the tables into main and minor tables. Main tables have a primary key _id column that is being referenced by other tables. eg _id from message is being referenced as message_row_id by other tables
@@ -84,7 +88,7 @@ base_tables_str=$(sqlite3 "$output" "select group_concat('''' || name || '''') f
 IFS=$'\n' read -rd '' -a m_tables <<< $(sqlite3 "$output" "select name from sqlite_master where type='table' and lower(name) in ('jid','chat','message','message_add_on','call_log','quick_replies','message_vcard','group_participant_user','labels','reporting_info');")
 
 
-#create indices temporarily in base database to prevent duplicates
+#create indices temporarily for base tables to prevent duplicates
 
 for table in "${m_tables[@]}"; do
  case $table in
@@ -139,63 +143,80 @@ for table in "${base_tables[@]}"; do
 done
 
 
-#remove base database from database array
-for ((i=0;i<${#dbs[@]};i++)); do
- if [[ ${dbs[i]} == "$base_db" ]]; then
-  unset dbs[i]
-  break
- fi
-done
-
 #to add the merged database to the array for sorting messages in the end
 dbs+=("$output")
 
 # check common columns
 function CheckCommonCols() {
   
-  common_columns=$(sqlite3 "$1" "select group_concat(name) as cols from pragma_table_info('$2') where name in ("${base_table_columns["$2"]}") and (name != '_id' or not exists (select 1 from pragma_table_info('$2') where name = '_id' and cid = 0));")
+  common_columns=$(sqlite3 "$1" "select group_concat(name) from pragma_table_info('$2') where name in ("${base_table_columns["$2"]}") and (name != '_id' or not exists (select 1 from pragma_table_info('$2') where name = '_id' and cid = 0));")
  if [[ -n "$common_columns" ]]; then 
   IFS="," read -ra column_list <<< "${common_columns}"
   column_list_select=()
   column_list_str=()
+ 
   for ((i=0;i<${#column_list[@]};i++)); do
-   if [[ "${column_list[i]}" == *"jid_row_id"* || "${column_list[i]}" == "business_owner_jid" || "${columns[i]}" == "seller_jid" || "${columns[i]}" == *"lid_row_id"* ]]; then
-    column_list_select[i]="j${i}.new_id"
-     if [[ "${column_list[i]}" == *"sender"* || "${column_list[i]}" == *"account"* || (("${column_list[i]}" == *"group_jid_row_id"* || "${column_list[i]}" == *"call_creator"* ) && "$2" == "call_log") ]]; then
-      column_list_str+=("left join jid_map77 j${i} on j${i}.old_id=x.${column_list[i]}")
+
+   case "${column_list[i]}" in
+    *jid_row_id*|business_owner_jid|seller_jid|*lid_row_id*)
+    column_list_select[i]="case when j${i}.new_id is null then x.${column_list[i]} else j${i}.new_id end"
+    column_list_str+=("left join jid_map77 j${i} on j${i}.old_id=x.${column_list[i]}")
+    ;;
+
+   *chat_row_id*)
+    column_list_select[i]="case when c${i}.new_id is null then x.${column_list[i]} else c${i}.new_id end"
+    column_list_str+=("left join chat_map77 c${i} on c${i}.old_id=x.${column_list[i]}")
+    ;;
+
+   *message_row_id*|*message_table_id*)
+     if [[ "$2" == "chat" ]]; then
+      column_list_select[i]="x.${column_list[i]}"
      else
-      column_list_str+=("join jid_map77 j${i} on j${i}.old_id=x.${column_list[i]}")
+      column_list_select[i]="case when m${i}.new_id is null then x.${column_list[i]} else m${i}.new_id end"
+      column_list_str+=("left join message_map77 m${i} on m${i}.old_id=x.${column_list[i]}")
      fi
-   elif [[ "${column_list[i]}" == *"chat_row_id"* ]]; then
-    column_list_select[i]="c${i}.new_id"
-    column_list_str+=("join chat_map77 c${i} on c${i}.old_id=x.${column_list[i]}")
-   elif [[ ("${column_list[i]}" == *"message_row_id"* || "${column_list[@]}" == *"message_table_id"*) && "$2" != "chat" ]]; then
-    column_list_select[i]="m${i}.new_id"
-    column_list_str+=("join message_map77 m${i} on m${i}.old_id=x.${column_list[i]}")
-   elif [[ "${column_list[i]}" == *"call_log_row_id"* || "${column_list[i]}" == *"call_logs_row_id"* ]]; then
-    column_list_select[i]="cl${i}.new_id"
-    column_list_str+=("join call_log_map77 cl${i} on cl${i}.old_id=x.${column_list[i]}")
-   elif [[ "${column_list[i]}" == *"message_add_on_row_id"* ]]; then
-    column_list_select[i]="m_add${i}.new_id"
-    column_list_str+=("join message_add_on_map77 m_add${i} on m_add${i}.old_id=x.${column_list[i]}")
-   elif [[ "${column_list[i]}" == *"vcard_row_id"* ]]; then
-    column_list_select[i]="vc${i}.new_id"
+    ;;
+
+   *call_log_row_id*|*call_logs_row_id*)
+    column_list_select[i]="case when cl${i}.new_id is null then x.${column_list[i]} else cl${i}.new_id end"
+    column_list_str+=("left join call_log_map77 cl${i} on cl${i}.old_id=x.${column_list[i]}")
+    ;;
+
+   *message_add_on_row_id*)
+    column_list_select[i]="case when m_add${i}.new_id is null then x.${column_list[i]} else m_add${i}.new_id end"
+    column_list_str+=("left join message_add_on_map77 m_add${i} on m_add${i}.old_id=x.${column_list[i]}")
+    ;;
+
+   *vcard_row_id*)
+    column_list_select[i]="case when vc${i}.new_id is null then x.${column_list[i]} else vc${i}.new_id end"
     column_list_str+=("left join message_vcard_map77 vc${i} on vc${i}.old_id=x.${column_list[i]}")
-   elif [[ "${column_list[i]}" == *"quick_reply_id"* ]]; then
-    column_list_select[i]="qr${i}.new_id"
-    column_list_str+=("join quick_replies_map77 qr${i} on qr${i}.old_id=x.${column_list[i]}")
-   elif [[ "${column_list[i]}" == *"reporting_info_row_id"* ]]; then
-    column_list_select[i]="ri${i}.new_id"
-    column_list_str+=("join reporting_info_map77 ri${i} on ri${i}.old_id=x.${column_list[i]}")
-   elif [[ "${column_list[i]}" == *"label_id"* ]]; then
-    column_list_select[i]="lb${i}.new_id"
-    column_list_str+=("join labels_map77 lb${i} on lb${i}.old_id=x.${column_list[i]}")
-   elif [[ "${column_list[i]}" =~ group_participant.*_row_id ]]; then
-    column_list_select[i]="gp${i}.new_id"
-    column_list_str+=("join group_participant_user_map77 gp${i} on gp${i}.old_id=x.${column_list[i]}")
-   else
+    ;;
+
+   *quick_reply_id*)
+    column_list_select[i]="case when qr${i}.new_id is null then x.${column_list[i]} else qr${i}.new_id end"
+    column_list_str+=("left join quick_replies_map77 qr${i} on qr${i}.old_id=x.${column_list[i]}")
+     ;;
+
+   *reporting_info_row_id*)
+    column_list_select[i]="case when ri${i}.new_id is null then x.${column_list[i]} else ri${i}.new_id end"
+    column_list_str+=("left join reporting_info_map77 ri${i} on ri${i}.old_id=x.${column_list[i]}")
+     ;;
+
+    *label_id*)
+     column_list_select[i]="case when lb${i}.new_id is null then x.${column_list[i]} else lb${i}.new_id end"
+     column_list_str+=("left join labels_map77 lb${i} on lb${i}.old_id=x.${column_list[i]}")
+     ;;
+
+    group_participant*_row_id)
+     column_list_select[i]="case when gp${i}.new_id is null then x.${column_list[i]} else gp${i}.new_id end"
+     column_list_str+=("left join group_participant_user_map77 gp${i} on gp${i}.old_id=x.${column_list[i]}")
+     ;;
+
+   *)
     column_list_select[i]="x.${column_list[i]}"
-   fi
+     ;;
+   esac
+
     mapjoin=""
     case "$table" in
      chat)
@@ -231,11 +252,12 @@ function CheckCommonCols() {
   common_columns_str=$(echo "${column_list_str[*]}")
   common_columns_str_select=$(IFS=","; echo "${column_list_select[*]}")
 
-  if [[ "$2" == "message" || "$2" == "call_log" || "$2" == "message_add_on" ]]; then
-  sortstr=" order by x.timestamp asc"
-  else
-  sortstr=""
-  fi
+# to sort a main table using timestamp add it here 
+  case "$2" in message|call_log|message_add_on)
+   sortstr=" order by x.timestamp asc" ;;
+  *)
+   sortstr="" ;;
+  esac
  fi   
 }
 
@@ -250,17 +272,19 @@ if [[ $count == ${#dbs[@]} ]]; then
   db="$output"
   output="$output_copy"
   echo "sorting messages"
+else
+  echo "merging"
 fi
 
 
  #insert jid
  CheckCommonCols "$db" "jid"
 
- sqlite3 "$output" "attach '$db' as db;" "insert or ignore into jid ("${common_columns}") select "${common_columns}" from db.jid;" "create table if not exists jid_map77 (old_id integer unique, new_id integer unique);" "delete from jid_map77;" "insert into jid_map77 (old_id, new_id) values (0,0);" "insert or ignore into jid_map77 (old_id,new_id) select dj._id,j._id from jid j join db.jid dj on j.raw_string=dj.raw_string;"
+ sqlite3 "$output" "attach '$db' as db;" "insert or ignore into jid ("${common_columns}") select "${common_columns}" from db.jid;" "create table if not exists jid_map77 (old_id integer unique, new_id integer unique);" "delete from jid_map77;" "insert or ignore into jid_map77 (old_id,new_id) select dj._id,j._id from jid j join db.jid dj on j.raw_string=dj.raw_string;"
 
 #checks for common main and minor tables
  
- IFS=$'\n' read -rd '' -a main_tables <<< $(sqlite3 "$db" "select name from sqlite_master where type='table' and name in ('chat','message','message_add_on','call_log','quick_replies','message_vcard','group_participant_user','labels','reporting_info') and name in ("${base_tables_str}") ORDER BY CASE name WHEN 'chat' THEN 1 WHEN 'message' THEN 2 WHEN 'message_add_on' THEN 3 WHEN 'call_log' THEN 4 WHEN 'quick_replies' THEN 5 WHEN 'message_vcard' THEN 6 WHEN 'group_participant_user' THEN 7 WHEN 'labels' THEN 8 WHEN 'reporting_info' then 9 END;")
+ IFS=$'\n' read -rd '' -a main_tables <<< $(sqlite3 "$db" "select name from sqlite_master where type='table' and name in ('chat','message','message_add_on','call_log','quick_replies','message_vcard','group_participant_user','labels','reporting_info') and name in ("${base_tables_str}") order by case name when 'chat' then 1 when 'message' then 2 when 'message_add_on' then 3 when 'call_log' then 4 when 'quick_replies' then 5 when 'message_vcard' then 6 when 'group_participant_user' then 7 when 'labels' then 8 when 'reporting_info' then 9 end;")
 
  IFS=$'\n' read -rd '' -a common_minor_tables <<< $(sqlite3 "$db" "select name from sqlite_master where type='table' and name in ("${base_tables_str}") and name not in ('jid','chat','message','message_add_on','call_log','quick_replies','message_vcard','group_participant_user','labels','reporting_info');")
  
@@ -269,14 +293,13 @@ fi
    
    CheckCommonCols "$db" "$table"
    if [[ -n "$common_columns" ]]; then
-    
     sqlite3 "$output" "attach '$db' as db;" "insert or ignore into ${table} (${common_columns}) select ${common_columns_str_select} from db.${table} x ${common_columns_str}${sortstr}" "create table if not exists ${table}_map77 (old_id integer unique, new_id integer unique);" "delete from ${table}_map77;" "insert or ignore into ${table}_map77 (old_id,new_id) ${mapjoin};"
+
    else
     echo -e "no common columns found in table $table on database: $output and database: $db\nThis table will be skipped"
    fi
  done
   
-
     
  # check common columns
  for table in "${common_minor_tables[@]}"; do
@@ -284,6 +307,8 @@ fi
   if [[ -n "$common_columns" ]]; then
  
    sqlite3 "$output" "attach '$db' as db;" "insert or ignore into ${table} (${common_columns}) select ${common_columns_str_select} from db.${table} x ${common_columns_str}"
+   
+   
    else
     echo -e "no common columns found in table $table on database: $output and database: $db\nThis table will be skipped"
    fi
@@ -297,10 +322,10 @@ echo "fixing chats"
 #fixing display ids in chat
 chat_update_string=$(sqlite3 "$db" "select group_concat(name || '= (select maxid from display_ids where display_ids.chatid = chat._id)') from pragma_table_info('chat') where (name like '%message_row_id%' or name like '%message_sort_id%') and name not like '%ephemeral%';")
 
-sqlite3 "$output_copy" "with display_ids as (select max(_id) as maxid,chat_row_id as chatid from message where message_type not in (0,7) or (message_type=0 and text_data is not null) group by chat_row_id) update chat set ${chat_update_string} where exists (select 1 from display_ids where display_ids.chatid = chat._id);"
+
+sqlite3 "$output_copy" "with display_ids as (select max(_id) as maxid,chat_row_id as chatid from message where message_type not in (0,7) or (message_type=0 and text_data is not null) group by chat_row_id) update chat set ${chat_update_string} where exists (select 1 from display_ids where display_ids.chatid = chat._id);" "update chat set hidden=case when exists (select 1 from message where chat_row_id = chat._id and message_type != 7) then 0 else 1 end;" "update chat set last_message_reaction_row_id=null,last_seen_message_reaction_row_id=null;" "with timestamps as (select chat_row_id as chat_id, max(timestamp) as latest from message where message_type !=7 group by chat_row_id) update chat set sort_timestamp=(select latest from timestamps where timestamps.chat_id=chat._id) where hidden=0 and exists (select 1 from timestamps where chat._id=timestamps.chat_id);"
 
 
-sqlite3 "$output_copy" "update chat set hidden=case when exists (select 1 from message where chat_row_id = chat._id and message_type != 7) then 0 else 1 end;" "update chat set last_message_reaction_row_id=null,last_seen_message_reaction_row_id=null;"
 
 
 #fixing props
@@ -310,7 +335,7 @@ sqlite3 "$output_copy" "delete from props where key='fts_index_start';" "update 
 
 
 #duplicates
-sqlite3 "$output_copy" "with freqs as (select row_number() over (partition by jid order by message_count desc) as rn, jid as j_id,message_count as m_count from frequents) delete from frequents where jid in (select j_id from freqs where rn>1);" "with freq as (select row_number() over (partition by jid_row_id order by message_count desc) as rn, jid_row_id as j_id,message_count as m_count from frequent) delete from frequent where jid_row_id in (select j_id from freq where rn>1);"
+sqlite3 "$output_copy" "delete from frequent where _id in (with freq as (select row_number() over (partition by jid_row_id order by message_count desc) as rn, _id, jid_row_id from frequent) select _id from freq where rn > 1);" "delete from frequents where _id in (with freqs as (select row_number() over (partition by jid order by message_count desc) as rn, _id, jid from frequents) select _id from freqs where rn > 1);"
 
 sqlite3 "$output_copy" "update message set sort_id=_id;"
 
@@ -319,12 +344,12 @@ sqlite3 "$output_copy" "update message set sort_id=_id;"
 echo "restoring triggers"
 #restore triggers
 
-printf "%s\n" "$triggers" | while IFS= read -r trigger_sql; do
- sqlite3 "$output_copy" "$trigger_sql"
+printf "%s\n" "$triggers" | while IFS= read -r trigger; do
+ sqlite3 "$output_copy" "${trigger#*|}" 2>/dev/null
 done
 
 
-echo "removing temp tables and indices"
+echo "removing temp tables and indexes"
 #delete map tables and temp indices
 
 IFS=$'\n' read -rd '' -a map_indices <<<$(sqlite3 "$output_copy" "select name from sqlite_master where name like '%_map77' or name like '%unique_index_77' and type in ('table','index');")
